2b78d7bdb60521b2a9c1c6da2e6a560b820ff397
SangYong-Jeong <mmaf@naver.com>
2021-10-08 15:02:40 +0900

    object reference

--- src/api/api-daily.js
+++ src/api/api-daily.js
@@ -1,13 +1,19 @@
-import axios from ''
+import axios from 'axios'
 
 const appid = '7f65332e6d33816b773f35daec5e2d44'
 const url = 'https://api.openweathermap.org/data/2.5/weather'
 // const icons = ['http://openweathermap.org/img/wn/', '@2x.png']
 const params = { units: 'metric', appid }
 
-const apiDaily = (options) => {
-  params.lat = options.lat
-  params.lon = options.lon
+const apiDaily = options => {
+  if (options.id) {
+    params.id = options.id
+  } else if (options.lat && options.lon) {
+    params.lat = options.lat
+    params.lon = options.lon
+  } else {
+    throw new Error('Error')
+  }
   return axios.get(url, { params })
 }
 
