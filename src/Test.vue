82424e6a70ee848069721048f3d38cd2017b1c92
SangYong-Jeong <mmaf@naver.com>
2021-10-12 17:42:25 +0900

    logic 계속 파악중

--- src/Test.vue
+++ src/Test.vue
@@ -1,64 +1,35 @@
 <template>
   <div>
-    <div>{{c}}</div>
-    <div>{{b}}</div>
-    <div>{{a}}</div>
+    <div>{{ GET_COORDS.a }}</div>
+    <div>{{ b }}</div>
+    <div>{{ c }}</div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  name: 'Test',
   data () {
-    console.log('hi-data')
+    console.log('data')
     return {
-      a: 0,
-      b: 1,
-      c: 2
+      b: 'hi2'
+    }
+  },
+  computed: {
+    ...mapGetters(['GET_COORDS', 'GET_DAILY']),
+    c: function () {
+      console.log('c-computed')
+      return 'hi3'
     }
   },
   created () {
+    this.GET_COORDS.a = 'hi'
     console.log('created')
-    console.log(this.a)
-    console.log(this.b)
-    console.log(this.c)
-    console.log(this.a1)
-    console.log(this.b1) // 각각이 computed 된 이후에 다시 created 실행 이 변수 관련된 부분만 실행된다.
-    console.log(this.c1) // 각각이 computed 된 이후에 다시 created 실행 이 변수 관련된 부분만 실행된다.
-    this.a = this.a + 3
-  },
-  computed: {
-    a1: function () {
-      console.log('hi-computed')
-      return this.a
-    },
-    b1: function () {
-      console.log('hi-computed2')
-      return this.b
-    },
-    c1: function () {
-      console.log('hi-computed3')
-      return this.c
-    }
-  },
-  watch: {
-    a: function () {
-      console.log('hi-watch1')
-      console.log(this.a)
-    },
-    b: function () {
-      console.log('hi-watch2')
-    },
-    c: function () {
-      console.log('hi-watch3')
-    },
-    a1: function () {
-      console.log('hi-watch4')
-    }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
